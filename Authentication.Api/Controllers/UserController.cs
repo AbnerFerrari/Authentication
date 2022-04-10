9d23d1b2fa567177b4c989971f8705fd6cd8ac8e
Abner Ferrari <abnerxperia@gmail.com>
2022-04-09 21:36:59 -0300

    Ajustes

---
+++ Authentication.Api/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IService<User> _service;
+        public UserController(IService<User> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(long id)
+        {
+            var entity = await _service.Get(id);
+            return Ok();
+        }
+    }
+}
